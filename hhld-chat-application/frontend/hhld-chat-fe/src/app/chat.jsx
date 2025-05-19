ef27658d55f89a46ed16d7437b75b6e1f66061eb
Gali Bhaskar <luciferbhaskar@gmail.com>
2025-05-19 13:40:19 -0700

    Added one-to-one chat frontend functionality

---
+++ hhld-chat-application/frontend/hhld-chat-fe/src/app/chat.jsx
@@ -0,0 +1,146 @@
+"use client";
+import React, { useState, useEffect, useCallback } from "react";
+import io from "socket.io-client";
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  const [msg, setMsg] = useState("");
+
+  const [socket, setSocket] = useState(null);
+
+  const createSocket = useCallback(() => {
+    console.log("create socket function");
+
+    if (socket) {
+      socket.disconnect();
+
+      setSocket(null);
+    }
+
+    const newSocket = io("http://localhost:8080");
+
+    newSocket.on("chat msg", (msg) => {
+      setMessages((prevMsgs) => [...prevMsgs, msg]);
+    });
+
+    setSocket(newSocket);
+  }, [socket]);
+
+  useEffect(() => {
+    // Establish WebSocket connection
+    createSocket();
+
+    return () => {
+      console.log("clean up");
+
+      if (socket) {
+        socket.disconnect();
+
+        setSocket(null);
+      }
+    };
+  }, []);
+
+  const sendMsg = (e) => {
+    e.preventDefault();
+
+    if (socket) {
+      // send message with event 'chat msg' and receive response for ack.
+      socket.emit("chat msg", msg, (res) => {
+        console.log("response from server:" + res);
+      });
+
+      setMessages((prevMsgs) => [...prevMsgs, msg]);
+
+      setMsg("");
+    }
+  };
+
+  const handleChange = (event) => {
+    setMsg(event.target.value);
+  };
+
+  const handleDisconnect = () => {
+    if (socket) {
+      socket.disconnect();
+
+      setSocket(null);
+
+      console.log("disconnected");
+    }
+  };
+
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="msgs-container h-4/5 overflow-scroll">
+        {messages.map((msg, index) => (
+          <div key={index} className="m-5 text-right">
+            {msg}
+          </div>
+        ))}
+      </div>
+      <div>
+        <form onSubmit={sendMsg} className="max-w-md mx-auto my-10">
+          <div className="relative">
+            <input
+              type="text"
+              value={msg}
+              onChange={handleChange}
+              placeholder="Type your text here"
+              required
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg
+bg-gray-50 focus:ring-blue-500 focus:border-blue-500
+dark:bg-gray-700 dark:border-gray-600
+dark:placeholder-gray-400 dark:text-white
+dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+
+            {socket == null ? (
+              <button
+                type="button"
+                onClick={() => {
+                  createSocket();
+                }}
+                className="text-white absolute end-2.5
+bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4
+focus:outline-none focus:ring-green-300 font-medium
+rounded-lg text-sm px-4 py-2 dark:bg-green-600
+dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
+                Connect
+              </button>
+            ) : (
+              <div>
+                <button
+                  type="submit"
+                  className="text-white absolute end-30
+bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4
+focus:outline-none focus:ring-blue-300 font-medium
+rounded-lg text-sm px-4 py-2 dark:bg-blue-600
+dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Send
+                </button>
+
+                <button
+                  type="button"
+                  onClick={handleDisconnect}
+                  className="text-white absolute end-2.5
+bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4
+focus:outline-none focus:ring-red-300 font-medium
+rounded-lg text-sm px-4 py-2 dark:bg-red-600
+dark:hover:bg-red-700 dark:focus:ring-red-800"
+                >
+                  Disconnect
+                </button>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
