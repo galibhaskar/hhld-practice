a12e48257268573327f6221d620fe60daba43248
Gali Bhaskar <luciferbhaskar@gmail.com>
2025-06-12 09:56:33 -0700

    Integrated Adaptive Bitrate Streaming using HLS(HTTP Live Streaming) to transcoding service

--- hhld-youtube-app/transcoder-service/index.js
+++ hhld-youtube-app/transcoder-service/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import KafkaConfig from "./kafka/kafka.js";
+import convertToHLS from "./hls/transcode.js";
+import s3ToS3 from "./hls/s3tos3.js";
 
 dotenv.config();
 
@@ -21,6 +23,24 @@ const kafkaConfigInstance = new KafkaConfig();
 
 kafkaConfigInstance.consume("transcode", (message) => {
   console.log("Received at Transcoding Service:", message);
+  let { title, url } = JSON.parse(message);
+
+  console.log("title:", title);
+
+  console.log("url:", url);
+
+  s3ToS3(title, url);
+
+  console.log("transcoding completed");
+});
+
+app.get("/transcode", (req, res) => {
+  console.log("request received");
+
+  convertToHLS();
+  // s3ToS3();
+
+  res.send("transcoding complete");
 });
 
 app.listen(PORT, () => {
